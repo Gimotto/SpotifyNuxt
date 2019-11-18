57f972b13977ff312fb7c0b4ebdd75159e8eb075
Giuseppe Marinelli <marinelli.giuseppe01@gmail.com>
2019-11-18 14:22:36 +0100

    premerge2

--- store/login.js
+++ store/login.js
@@ -12,23 +12,7 @@ export const state = () => ({
            state.token = string
            this.$cookies.set("auth_token", state.token); 
     }
-  
-}
-export const mutations = {
-  setState(state,string){
-    state.token = string
-    this.$cookies.set('auth_token', string)
-  },
-  /*
-  changeTokenValidation(state, value){
-    state.tokenValid = value;
-  }
-  */
 }
 export const action = {
-  /*
-  changeTokenValidation(e, value){
-    e.commit('changeTokenValidation', value)
-  }
-  */
+
 }
